a0384b686f0aeff8d03d6079ae3477a7c26e191a
Mike Prap <michaelpraplaski@yahoo.com>
2019-10-09 19:48:58 -0400

    created friendArray

---
+++ app/data/friends.js
@@ -0,0 +1,58 @@
+// create an array of friends
+
+var friendArray = [
+{
+    name: "Captain America",
+    photo:"https://static0.srcdn.com/wordpress/wp-content/uploads/2019/05/Chris-Evans-as-Captain-America-in-the-Quantum-Realm.jpg",
+    scores: [
+        3,
+        5,
+        1,
+        2,
+        1,
+        4,
+        5,
+        2,
+        2,
+        3,
+    ]
+},
+{
+    name: "Iron Man",
+    photo:"https://cdn.pinkvilla.com/files/styles/contentpreview/public/iron-man-phase-4_0.jpg?itok=zzbN51r7",
+    scores: [
+        1,
+        2,
+        3,
+        3,
+        4,
+        1,
+        5,
+        1,
+        2,
+        1,
+    ]
+},
+{
+    name: "Thor",
+    photo:"https://cdn3.movieweb.com/i/article/6WqYgaKiWvpZLH3diGW88RrhK7lmVR/798:50/Thor-Rewritten-Infinity-War-Endgame-Ragnarok.jpg",
+    scores: [
+        1,
+        1,
+        1,
+        5,
+        5,
+        5,
+        1,
+        1,
+        5,
+        5,
+    ]
+},
+    
+
+
+
+
+
+]
