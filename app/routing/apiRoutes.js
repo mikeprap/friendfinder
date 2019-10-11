d63e9c0abd4b2c2560f87b2d4dbfcbd4d6fd4511
Mike Prap <michaelpraplaski@yahoo.com>
2019-10-11 06:44:26 -0400

    fixed problems with my js on the survey.html

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,22 +7,13 @@ var friendArray = require("../data/friends");
 
 module.exports = function(app){
 
-        // 
+        
     app.get("/api/friends", function(req, res){
         res.json(friendArray)
 
     })
 
-    app.post("/api/friends", function (req, res){
-        var newPerson = req.body;
-        
-        for(var i = 0; i < newPerson.scores.length;i++){
-            newPerson.scores[i] = parseInt(newPerson.scores[i]);
-        }
-
-        console.log(newPerson.scores);
-        
-    })
+    
     app.post("/api/friends", function(req, res){
 
     
